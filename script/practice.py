ca8662e93c1d0a5b45768a43218dc4ea569ae094
hereisdavidgan <571825401@qq.com>
2022-01-23 14:39:19 +0800

    2

--- script/practice.py
+++ script/practice.py
@@ -1,27 +1,13 @@
-import csv
 import random
 
-# with open('givenname.csv', 'r') as f:
-#     reader = csv.reader(f)
-#     print(type(reader))
-number = int(input('How Many People You Wannan Creat:'))
-givenname1 = csv.reader(open('..\\csv\\givenname.csv', mode='r', encoding='utf-8'))
-firstname1 = csv.reader(open('..\\csv\\firstname.csv', mode='r', encoding='utf-8'))
-givenname = []
-firstname = []
-
-for i in givenname1:
-    givenname += i
-for i in firstname1:
-    firstname += i
-lg = len(givenname)
-lf = len(firstname)
-for i in range(number):
-    if i % 10 == 0:
-        name = firstname[random.randint(0, lf)]
-        name += givenname[random.randint(0, lg)]
-    else:
-        name = firstname[random.randint(0, lf)]
-        name += givenname[random.randint(0, lg)]
-        name += givenname[random.randint(0, lg)]
-    print(name)
+list1 = ['jituan1', 'jituan2', 'jituan3']
+list2 = ['yewuqun1', 'yewuqun2', 'yewuqun3', 'yewuqun4', 'yewuqun5']
+while len(list2) > len(list1):
+    list1.append(random.choice(list1))
+print(list1)
+random.shuffle(list1)
+random.shuffle(list2)
+dict1 = zip(list2, list1)
+# dict2 = {'业务群1': '集团1', '业务群2': '集团2'}
+# dict2.update(dict1)
+print(dict1)
